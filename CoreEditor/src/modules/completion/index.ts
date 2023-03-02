2967153b6b7f42dc2ce82f5e50fe8947b020e905
Gnohz Gniy <0x00eeee@gmail.com>
2023-03-02 16:13:53 +0800

    Implement startCompletion for web module

--- CoreEditor/src/modules/completion/index.ts
+++ CoreEditor/src/modules/completion/index.ts
@@ -1,5 +1,43 @@
+import { editingState } from '../../common/store';
+import anchorAtPos from '../tokenizer/anchorAtPos';
+
 export function startCompletion() {
-  // tbd
+  if (storage.cancellable !== undefined) {
+    clearTimeout(storage.cancellable);
+  }
+
+  // Don't trigger completion for every keystroke,
+  // instead we delay the request cancel previously scheduled ones.
+  storage.cancellable = setTimeout(() => {
+    // We don't want to trigger completion when composition is still ongoing,
+    // marked text in input methods like Pinyin is not meaningful until composition is ended.
+    if (!editingState.compositionEnded) {
+      return;
+    }
+
+    const editor = window.editor;
+    const state = editor.state;
+    const pos = state.selection.main.anchor;
+
+    const fullText = (() => {
+      const position = editor.coordsAtPos(pos)?.top ?? 0;
+      const usesCache = Math.abs(position - storage.cachedPosition) < 5;
+      storage.cachedPosition = position;
+
+      // If y-axis isn't changed noticeably, it means that we are keep working on the same line.
+      // In that case, we skip sending full text for tokenization to improve the performance.
+      //
+      // This is not sound, and we don't want a sound but super complicated algorithm.
+      if (usesCache) {
+        return undefined;
+      } else {
+        return state.doc.toString();
+      }
+    })();
+
+    const anchor = anchorAtPos(pos);
+    window.nativeModules.completion.requestCompletions({ anchor, fullText });
+  }, 300);
 }
 
 export function setPanelVisible(visible: boolean) {
