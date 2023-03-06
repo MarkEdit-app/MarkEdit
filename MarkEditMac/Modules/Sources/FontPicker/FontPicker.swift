b4a8eda6989ae14c5bde6954577c20b4296ecbfb
Gnohz Gniy <0x00eeee@gmail.com>
2023-03-06 23:38:52 +0800

    Fix TextField bezel on Monterey (#92)

--- MarkEditMac/Modules/Sources/FontPicker/FontPicker.swift
+++ MarkEditMac/Modules/Sources/FontPicker/FontPicker.swift
@@ -29,9 +29,13 @@ public struct FontPicker: View {
     HStack {
       ZStack {
         // Just to steal the bezel UI from TextField
-        TextField(text: .constant("")) {}
-          .focusable(false)
-          .allowsHitTesting(false)
+        if #available(macOS 13.0, *) {
+          TextField(text: .constant("")) {}
+            .focusable(false) // Doesn't work on Monterey
+            .allowsHitTesting(false)
+        } else {
+          LegacyTextFieldBezel()
+        }
 
         Text(configuration.localizedInfo(style: selectedFontStyle, size: selectedFontSize))
           .font(Font(selectedFontStyle.fontWith(size: 12)))
