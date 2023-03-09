fff09a6d17f4b3d544c12fa02945f2d80a82f373
Gnohz Gniy <0x00eeee@gmail.com>
2023-03-09 21:08:08 +0800

    Change "text completion" to "word completion"

--- MarkEditMac/Sources/Editor/Controllers/EditorViewController+UI.swift
+++ MarkEditMac/Sources/Editor/Controllers/EditorViewController+UI.swift
@@ -28,7 +28,7 @@ extension EditorViewController {
     }
 
     // Press backspace or option to cancel the correction indicator,
-    // it ensures a smoother text completion experience.
+    // it ensures a smoother word completion experience.
     NSEvent.addLocalMonitorForEvents(matching: [.keyDown, .flagsChanged]) { [weak self] event in
       if (event.keyCode == 51 || event.keyCode == 58), let self {
         NSSpellChecker.shared.declineCorrectionIndicator(for: self.webView)
