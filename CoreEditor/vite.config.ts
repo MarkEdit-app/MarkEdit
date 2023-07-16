72aa2b9d3f2318cd33dd75b387db72dbc6e88984
Gnohz Gniy <0x00eeee@gmail.com>
2023-07-16 10:54:07 +0800

    Suppress vite warning for dynamic import (#221)

--- CoreEditor/vite.config.ts
+++ CoreEditor/vite.config.ts
@@ -1,6 +1,21 @@
-import { defineConfig } from 'vite';
+import { createLogger, defineConfig } from 'vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
+  customLogger: (() => {
+    const logger = createLogger();
+    const warn = logger.warn;
+
+    logger.warn = (message, options) => {
+      // Ignore CodeMirror dynamic import warnings since we are not going to do anything (#214)
+      if (message.includes('@vendor/language-data') && message.includes('dynamic import will not move module into another chunk')) {
+        return;
+      }
+
+      warn(message, options);
+    };
+
+    return logger;
+  })(),
   plugins: [viteSingleFile()],
 });
