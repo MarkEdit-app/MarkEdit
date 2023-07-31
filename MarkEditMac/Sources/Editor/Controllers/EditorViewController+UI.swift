99965ffde0cf2c71435f37d41ddf15e2c1da1b82
Gnohz Gniy <0x00eeee@gmail.com>
2023-07-31 12:56:14 +0800

    Press tab key to accept spellcheck suggestion (#245)

--- MarkEditMac/Sources/Editor/Controllers/EditorViewController+UI.swift
+++ MarkEditMac/Sources/Editor/Controllers/EditorViewController+UI.swift
@@ -29,11 +29,16 @@ extension EditorViewController {
       view.needsLayout = true
     }
 
-    // Press backspace or option to cancel the correction indicator,
-    // it ensures a smoother word completion experience.
     NSEvent.addLocalMonitorForEvents(matching: [.keyDown, .flagsChanged]) { [weak self] event in
+      // Press backspace or option to cancel the correction indicator,
+      // it ensures a smoother word completion experience.
       if (event.keyCode == 51 || event.keyCode == 58), let self {
         NSSpellChecker.shared.declineCorrectionIndicator(for: self.webView)
+      }
+
+      // Press tab key to accept the first spellcheck suggestion
+      if event.keyCode == 48, let self {
+        NSSpellChecker.shared.dismissCorrectionIndicator(for: self.webView)
       }
 
       return event
