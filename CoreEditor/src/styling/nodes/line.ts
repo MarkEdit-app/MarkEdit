8ccd043e5910f4e4af8cadd42db9a98c3de43c9f
Gnohz Gniy <0x00eeee@gmail.com>
2023-08-14 11:49:43 +0800

    Hide left and right borders

--- CoreEditor/src/styling/nodes/line.ts
+++ CoreEditor/src/styling/nodes/line.ts
@@ -1,6 +1,8 @@
 import { RectangleMarker, layer } from '@codemirror/view';
 import { buildInnerBorder } from '../builder';
 
+const borderWidth = 2.5;
+
 /**
  * Line level indentations make the background of active lines partially drawn,
  * use this home-made version to have full-width indicators.
@@ -38,15 +40,27 @@ class Layer extends RectangleMarker {
   constructor(content: HTMLElement, line: HTMLElement, private readonly theme?: string) {
     const lineRect = line.getBoundingClientRect();
     const contentRect = content.getBoundingClientRect();
-    super('cm-md-activeIndicator', contentRect.left, line.offsetTop, contentRect.width, lineRect.height);
+
+    // We use box shadow to draw inner borders, use an inset to hide the left and right borders
+    const borderInset = (() => {
+      if (window.colors?.lineBorder === undefined) {
+        return 0;
+      }
+
+      // Slightly bigger to avoid precision issues
+      return borderWidth + 1;
+    })();
 
     // The rect is wider than lineRect, it fills the entire contentDOM
-    this.rect = new DOMRect(
-      contentRect.left,   // x
-      line.offsetTop,     // y
-      contentRect.width,  // width
-      lineRect.height,    // height
+    const rectToDraw = new DOMRect(
+      contentRect.left - borderInset,       // x
+      line.offsetTop,                       // y
+      contentRect.width + borderInset * 2,  // width
+      lineRect.height,                      // height
     );
+
+    super('cm-md-activeIndicator', rectToDraw.left, rectToDraw.top, rectToDraw.width, rectToDraw.height);
+    this.rect = rectToDraw;
   }
 
   draw() {
@@ -75,7 +89,7 @@ class Layer extends RectangleMarker {
   private render(elt: HTMLElement) {
     const colors = window.colors;
     elt.style.backgroundColor = colors?.activeLine ?? '';
-    elt.style.boxShadow = buildInnerBorder(2.5, colors?.lineBorder);
+    elt.style.boxShadow = buildInnerBorder(borderWidth, colors?.lineBorder);
   }
 }
 
