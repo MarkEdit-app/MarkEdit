ebe9eafc8fd9083a019299169065fcb81ed61d33
Gnohz Gniy <0x00eeee@gmail.com>
2023-08-26 00:21:13 +0800

    Change presentPopover to private

--- MarkEditMac/Sources/Editor/Controllers/EditorViewController+Preview.swift
+++ MarkEditMac/Sources/Editor/Controllers/EditorViewController+Preview.swift
@@ -17,3 +17,24 @@ extension EditorViewController {
     presentPopover(popover, rect: rect)
   }
 }
+
+// MARK: - Private
+
+private extension EditorViewController {
+  func presentPopover(_ popover: NSPopover, rect: CGRect) {
+    if focusTrackingView.superview == nil {
+      webView.addSubview(focusTrackingView)
+    }
+
+    // The origin has to be inside the viewport, and the size cannot be zero
+    focusTrackingView.frame = CGRect(
+      x: max(0, rect.minX),
+      y: max(0, rect.minY),
+      width: max(1, rect.width),
+      height: max(1, rect.height)
+    )
+
+    presentedPopover = popover
+    popover.show(relativeTo: rect, of: focusTrackingView, preferredEdge: .maxX)
+  }
+}
