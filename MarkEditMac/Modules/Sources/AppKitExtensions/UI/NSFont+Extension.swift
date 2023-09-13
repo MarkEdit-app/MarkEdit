e1732e6bf083e5c8f309ccf2d3303c3788fe655f
Gnohz Gniy <0x00eeee@gmail.com>
2023-09-14 00:20:03 +0800

    Fix font synthesis issues on Mac (#341)

--- MarkEditMac/Modules/Sources/AppKitExtensions/UI/NSFont+Extension.swift
+++ MarkEditMac/Modules/Sources/AppKitExtensions/UI/NSFont+Extension.swift
@@ -7,6 +7,18 @@
 import AppKit
 
 public extension NSFont {
+  var cssFontFamily: String {
+    // If the font is bold or italic, we use its name directly,
+    // because we want to apply the style to all scenarios.
+    if isBoldOrItalic {
+      return fontName
+    }
+
+    // Otherwise, we use its family name to avoid font synthesis,
+    // which is buggy in WebKit on macOS, i.e., bold text gets bolder and blurry.
+    return familyName ?? fontName
+  }
+
   static func monospacedSystemFont(ofSize fontSize: Double) -> NSFont {
     monospacedSystemFont(ofSize: fontSize, weight: .regular)
   }
@@ -23,6 +35,11 @@ public extension NSFont {
 // MARK: - Private
 
 private extension NSFont {
+  var isBoldOrItalic: Bool {
+    let traits = fontDescriptor.symbolicTraits
+    return traits.contains(.bold) || traits.contains(.italic)
+  }
+
   func withDesign(_ design: NSFontDescriptor.SystemDesign) -> NSFont {
     guard let descriptor = fontDescriptor.withDesign(design) else {
       return self
