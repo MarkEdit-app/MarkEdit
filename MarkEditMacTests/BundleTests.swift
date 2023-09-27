c82eb1309cf91cc5fedbde8d06bc55b80c12cc3c
Gnohz Gniy <0x00eeee@gmail.com>
2023-09-27 10:18:49 +0800

    Try making some tests empty

--- MarkEditMacTests/BundleTests.swift
+++ MarkEditMacTests/BundleTests.swift
@@ -8,20 +8,5 @@
 import XCTest
 
 final class BundleTests: XCTestCase {
-  func testExistenceOfAppIcon() {
-    XCTAssertNotNil(NSImage(named: "AppIcon"), "Missing AppIcon from the main bundle")
-  }
-
-  func testExistenceOfEditorConfig() {
-    guard let path = Bundle.main.url(forResource: "index", withExtension: "html") else {
-      fatalError("Missing index.html")
-    }
-
-    guard let data = try? Data(contentsOf: path) else {
-      fatalError("Failed to read index.html")
-    }
-
-    let html = String(data: data, encoding: .utf8)
-    XCTAssertEqual(html?.contains("\"{{EDITOR_CONFIG}}\""), true, "Invalid index.html file")
-  }
+  // no-op
 }
