5fccf3ac5181187dbec7b69823b02776b2dcb5ee
Gnohz Gniy <0x00eeee@gmail.com>
2023-09-27 17:48:17 +0800

    Fix CI hangs on Xcode 15 (#361)
    * Try making some tests empty
    
    * Revert BundleTests
    
    * Try dummy test
    
    * Revert RuntimeTests
    
    * Try keep testExistenceOfAppIcon
    
    * Try XCTAssert instead of fatalError
    
    * Move to testExistenceOfEditorConfig to ts

---
+++ CoreEditor/test/build.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, test } from '@jest/globals';
+import fs from 'fs';
+import path from 'path';
+
+describe('Build system test suite', () => {
+  test('test existence of editor config', () => {
+    const testFileName = (fileName: string) => {
+      const html = fs.readFileSync(path.join(__dirname, fileName), 'utf-8');
+      expect(html).toContain('"{{EDITOR_CONFIG}}"');
+    };
+
+    testFileName('../dist/index.html');
+    testFileName('../src/@light/dist/index.html');
+  });
+});
--- MarkEditMacTests/BundleTests.swift
+++ MarkEditMacTests/BundleTests.swift
@@ -11,17 +11,4 @@ final class BundleTests: XCTestCase {
   func testExistenceOfAppIcon() {
     XCTAssertNotNil(NSImage(named: "AppIcon"), "Missing AppIcon from the main bundle")
   }
-
-  func testExistenceOfEditorConfig() {
-    guard let path = Bundle.main.url(forResource: "index", withExtension: "html") else {
-      fatalError("Missing index.html")
-    }
-
-    guard let data = try? Data(contentsOf: path) else {
-      fatalError("Failed to read index.html")
-    }
-
-    let html = String(data: data, encoding: .utf8)
-    XCTAssertEqual(html?.contains("\"{{EDITOR_CONFIG}}\""), true, "Invalid index.html file")
-  }
 }
