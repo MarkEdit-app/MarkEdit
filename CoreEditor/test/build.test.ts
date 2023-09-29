0144ed83977dc373c5ecad5207f874c64bb3bf98
Gnohz Gniy <0x00eeee@gmail.com>
2023-09-29 10:31:18 +0800

    Trigger

--- CoreEditor/test/build.test.ts
+++ CoreEditor/test/build.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from '@jest/globals';
 import fs from 'fs';
 import path from 'path';
 
-describe('Build system test suite', () => {
+describe('Build system', () => {
   test('test existence of editor config', () => {
     const testFileName = (fileName: string) => {
       const html = fs.readFileSync(path.join(__dirname, fileName), 'utf-8');
