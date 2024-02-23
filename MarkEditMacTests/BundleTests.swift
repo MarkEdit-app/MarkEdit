912c0c49c3689fa9b4aa13804f3fdcee2066a581
Gnohz Gniy <0x00eeee@gmail.com>
2024-02-23 19:22:51 +0800

    Revert "Try #7"
    This reverts commit 60be578d142d4f017f2b15f1a67828aa45d2e9c2.

---
+++ MarkEditMacTests/BundleTests.swift
@@ -0,0 +1,13 @@
+//
+//  BundleTests.swift
+//  MarkEditMacTests
+//
+//  Created by cyan on 2/2/23.
+//
+
+@testable import MarkEdit
+import XCTest
+
+final class BundleTests: XCTestCase {
+  //
+}
