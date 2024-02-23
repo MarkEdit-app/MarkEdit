6432ca9e465f685bf5b7595d627f2bdb403ceac3
Gnohz Gniy <0x00eeee@gmail.com>
2024-02-23 18:45:51 +0800

    Try #9

--- MarkEditMacTests/RuntimeTests.swift
+++ MarkEditMacTests/RuntimeTests.swift
@@ -11,7 +11,84 @@ import WebKit
 import AppKitExtensions
 
 final class RuntimeTests: XCTestCase {
-  //
+  func testExistenceOfDrawsBackground() {
+    let configuration = WKWebViewConfiguration()
+    configuration.setValue(false, forKey: "drawsBackground")
+    testExistenceOfSelector(object: configuration, selector: "_drawsBackground")
+  }
+
+  func testExistenceOfDeveloperExtras() {
+    let preferences = WKWebViewConfiguration().preferences
+    preferences.setValue(true, forKey: "developerExtrasEnabled")
+    testExistenceOfSelector(object: preferences, selector: "_developerExtrasEnabled")
+  }
+
+  func testExistenceOfAutomaticInlineCompletion() {
+    guard #available(macOS 14.0, *) else {
+      return
+    }
+
+    let checker = NSSpellChecker.self
+    testExistenceOfSelector(object: checker, selector: "isAutomaticInlineCompletionEnabled")
+  }
+
+  func testExistenceOfCancelCorrection() {
+    let checker = NSSpellChecker()
+    testExistenceOfSelector(object: checker, selector: "cancelCorrectionIndicatorForView:")
+  }
+
+  func testRetrievingPopover() {
+    class ContentViewController: NSViewController {
+      override func loadView() {
+        view = NSView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+      }
+    }
+
+    let window = NSWindow()
+    window.makeKeyAndOrderFront(nil)
+
+    guard let contentView = window.contentView else {
+      XCTAssert(false, "Missing contentView in NSWindow")
+      return
+    }
+
+    let popover = NSPopover()
+    popover.contentViewController = ContentViewController(nibName: nil, bundle: nil)
+    popover.show(
+      relativeTo: CGRect(x: 0, y: 0, width: 1, height: 1),
+      of: contentView,
+      preferredEdge: .maxX
+    )
+
+    let expectation = XCTestExpectation()
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+      expectation.fulfill()
+    }
+
+    wait(for: [expectation])
+    XCTAssertNotNil(popover.contentViewController?.view.window?.value(forKey: "_popover"))
+  }
+
+  func testRetrievingToolbarEffectView() {
+    let window = NSWindow()
+    window.makeKeyAndOrderFront(nil)
+    XCTAssertNotNil(window.toolbarEffectView)
+  }
+
+  func testPrivateAppKitClasses() {
+    testExistenceOfClass(named: "_NSKeyboardFocusClipView")
+    testExistenceOfClass(named: "NSToolbarFullScreenWindow")
+    testExistenceOfClass(named: "NSTitlebarView")
+    testExistenceOfClass(named: "NSToolbarButton")
+  }
+
+  func testPrivateAccessibilityBundles() {
+    let type: AnyClass? = NSObject.axbbmClass
+    XCTAssertNotNil(type, "Missing AXBBundleManager")
+
+    let object = type?.value(forKey: "defaultManager") as? AnyObject
+    XCTAssertEqual(object?.responds(to: sel_getUid("loadAXBundles")), true, "Missing loadAXBundles")
+  }
 }
 
 // MARK: - Private
