b0d283189efeb751d45e098a22de27b8ce07900c
Gnohz Gniy <0x00eeee@gmail.com>
2024-04-18 22:51:43 +0800

    SWIFT_STRICT_CONCURRENCY = complete

--- MarkEditMac/Sources/Main/AppPreferences.swift
+++ MarkEditMac/Sources/Main/AppPreferences.swift
@@ -14,6 +14,7 @@ import FontPicker
 /**
  UserDefaults wrapper with handy getters and setters.
  */
+@MainActor
 enum AppPreferences {
   enum General {
     @Storage(key: "general.appearance", defaultValue: .system)
@@ -52,18 +53,14 @@ enum AppPreferences {
     @Storage(key: "editor.light-theme", defaultValue: AppTheme.GitHubLight.editorTheme)
     static var lightTheme: String {
       didSet {
-        Task { @MainActor in
-          AppTheme.current.updateAppearance(animateChanges: true)
-        }
+        AppTheme.current.updateAppearance(animateChanges: true)
       }
     }
 
     @Storage(key: "editor.dark-theme", defaultValue: AppTheme.GitHubDark.editorTheme)
     static var darkTheme: String {
       didSet {
-        Task { @MainActor in
-          AppTheme.current.updateAppearance(animateChanges: true)
-        }
+        AppTheme.current.updateAppearance(animateChanges: true)
       }
     }
 
@@ -171,10 +168,8 @@ enum AppPreferences {
     @Storage(key: "assistant.inline-predictions", defaultValue: true)
     static var inlinePredictions: Bool {
       didSet {
-        Task { @MainActor in
-          NSSpellChecker.InlineCompletion.spellCheckerEnabled = inlinePredictions
-          performUpdates { $0.setInlinePredictions(enabled: inlinePredictions) }
-        }
+        NSSpellChecker.InlineCompletion.spellCheckerEnabled = inlinePredictions
+        performUpdates { $0.setInlinePredictions(enabled: inlinePredictions) }
       }
     }
 
@@ -283,6 +278,7 @@ enum Appearance: Codable {
   case light
   case dark
 
+  @MainActor
   func resolved(with appearance: NSAppearance = NSApp.effectiveAppearance) -> NSAppearance? {
     switch self {
     case .system:
@@ -366,14 +362,13 @@ extension NSWindow.TabbingMode: Codable {}
 
 private extension AppPreferences {
   static func performUpdates(action: @escaping (EditorViewController) -> Void) {
-    Task { @MainActor in
-      for editor in EditorReusePool.shared.viewControllers() {
-        action(editor)
-      }
+    for editor in EditorReusePool.shared.viewControllers() {
+      action(editor)
     }
   }
 }
 
+@MainActor
 @propertyWrapper
 struct Storage<T: Codable> {
   private let key: String
