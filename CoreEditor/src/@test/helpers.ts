8f1d6fce74468437f275538f4989687d5f649c4a
Gnohz Gniy <0x00eeee@gmail.com>
2024-05-13 21:28:44 +0800

    Add a small delay to acceptInlinePrediction

--- CoreEditor/src/@test/helpers.ts
+++ CoreEditor/src/@test/helpers.ts
@@ -1,6 +1,1 @@
-export function sleep(milliseconds: number) {
-  // eslint-disable-next-line compat/compat
-  return new Promise(resolve => {
-    setTimeout(resolve, milliseconds);
-  });
-}
+export { sleep } from '../common/utils';
