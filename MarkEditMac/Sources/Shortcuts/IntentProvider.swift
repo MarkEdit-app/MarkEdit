cb204f667465bab703310ee201318e8b3d512bf9
Gnohz Gniy <0x00eeee@gmail.com>
2024-06-11 22:10:38 +0800

    Bring back EvaluateJavaScriptIntent

--- MarkEditMac/Sources/Shortcuts/IntentProvider.swift
+++ MarkEditMac/Sources/Shortcuts/IntentProvider.swift
@@ -10,18 +10,30 @@ import AppIntents
 struct IntentProvider: AppShortcutsProvider {
   static var appShortcuts: [AppShortcut] {
     return [
-      AppShortcut(intent: CreateNewDocumentIntent(), phrases: [
-        "Create New Document in \(.applicationName)",
-      ]),
-      AppShortcut(intent: CreateNewDocumentIntent(), phrases: [
-        "Evaluate JavaScript in \(.applicationName)",
-      ]),
-      AppShortcut(intent: GetFileContentIntent(), phrases: [
-        "Get File Content in \(.applicationName)",
-      ]),
-      AppShortcut(intent: UpdateFileContentIntent(), phrases: [
-        "Update File Content in \(.applicationName)",
-      ]),
+      AppShortcut(
+        intent: CreateNewDocumentIntent(),
+        phrases: [
+          "Create New Document in \(.applicationName)",
+        ]
+      ),
+      AppShortcut(
+        intent: EvaluateJavaScriptIntent(),
+        phrases: [
+          "Evaluate JavaScript in \(.applicationName)",
+        ]
+      ),
+      AppShortcut(
+        intent: GetFileContentIntent(),
+        phrases: [
+          "Get File Content in \(.applicationName)",
+        ]
+      ),
+      AppShortcut(
+        intent: UpdateFileContentIntent(),
+        phrases: [
+          "Update File Content in \(.applicationName)",
+        ]
+      ),
     ]
   }
 }
