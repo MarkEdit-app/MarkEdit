c325da3ecd854f5de07f774d14efa376cc807973
Gnohz Gniy <0x00eeee@gmail.com>
2024-08-07 10:30:21 +0800

    Update dependencies

--- MarkEditTools/Package.swift
+++ MarkEditTools/Package.swift
@@ -15,8 +15,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "SwiftLintBinary",
-      url: "https://github.com/realm/SwiftLint/releases/download/0.55.1/SwiftLintBinary-macos.artifactbundle.zip",
-      checksum: "722a705de1cf4e0e07f2b7d2f9f631f3a8b2635a0c84cce99f9677b38aa4a1d6"
+      url: "https://github.com/realm/SwiftLint/releases/download/0.56.1/SwiftLintBinary-macos.artifactbundle.zip",
+      checksum: "146ef723e83d301b9f1ef647dc924a55dae293887e633618e76f8cb526292f0c"
     ),
     .plugin(
       name: "SwiftLint",
