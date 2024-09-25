9f1884a6aa328c8db8aa0964ff7f84c5e81f6a82
Gnohz Gniy <0x00eeee@gmail.com>
2024-09-25 10:31:25 +0800

    Fix recently introduced build errors

--- MarkEditMac/Sources/Editor/Controllers/EditorViewController.swift
+++ MarkEditMac/Sources/Editor/Controllers/EditorViewController.swift
@@ -234,9 +234,11 @@ final class EditorViewController: NSViewController {
   }()
 
   deinit {
-    if let monitor = localEventMonitor {
-      NSEvent.removeMonitor(monitor)
-      localEventMonitor = nil
+    MainActor.assumeIsolated {
+      if let monitor = localEventMonitor {
+        NSEvent.removeMonitor(monitor)
+        localEventMonitor = nil
+      }
     }
   }
 
