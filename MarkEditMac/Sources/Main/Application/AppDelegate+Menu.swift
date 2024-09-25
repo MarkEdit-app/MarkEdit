881f1a23260dc4624cbd19d937d6fb362bb31e42
Gnohz Gniy <0x00eeee@gmail.com>
2024-09-25 10:31:25 +0800

    Fix recently introduced build errors

--- MarkEditMac/Sources/Main/Application/AppDelegate+Menu.swift
+++ MarkEditMac/Sources/Main/Application/AppDelegate+Menu.swift
@@ -153,7 +153,10 @@ private extension AppDelegate {
 private extension AppDelegate {
   @IBAction func checkForUpdates(_ sender: Any?) {
     Task {
-      await AppUpdater.checkForUpdates(explicitly: true)
+      await AppUpdater.checkForUpdates(
+        explicitly: true,
+        skippedVersions: AppPreferences.Updater.skippedVersions
+      )
     }
   }
 
