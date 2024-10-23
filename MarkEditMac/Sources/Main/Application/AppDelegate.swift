0ea44bea2822fc547fa2ab467085306d66e0fb09
Gnohz Gniy <0x00eeee@gmail.com>
2024-10-23 08:23:55 +0800

    Lots of new (fucking) @unchecked Sendable

--- MarkEditMac/Sources/Main/Application/AppDelegate.swift
+++ MarkEditMac/Sources/Main/Application/AppDelegate.swift
@@ -10,7 +10,7 @@ import SettingsUI
 import MarkEditKit
 
 @MainActor
-final class AppDelegate: NSObject, NSApplicationDelegate {
+final class AppDelegate: NSObject, NSApplicationDelegate, @unchecked Sendable {
   @IBOutlet weak var mainFileMenu: NSMenu?
   @IBOutlet weak var mainEditMenu: NSMenu?
   @IBOutlet weak var mainExtensionsMenu: NSMenu?
@@ -92,6 +92,15 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
       Task { @MainActor in
         self.checkForUpdates(explicitly: false)
       }
+    }
+  }
+
+  func checkForUpdates(explicitly: Bool) {
+    Task {
+      await AppUpdater.checkForUpdates(
+        explicitly: explicitly,
+        skippedVersions: AppPreferences.Updater.skippedVersions
+      )
     }
   }
 }
