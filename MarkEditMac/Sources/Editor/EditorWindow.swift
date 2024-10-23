ad5a2b521d73001d65172fc5a266f86f1ee303d6
Gnohz Gniy <0x00eeee@gmail.com>
2024-10-23 08:23:55 +0800

    Lots of new (fucking) @unchecked Sendable

--- MarkEditMac/Sources/Editor/EditorWindow.swift
+++ MarkEditMac/Sources/Editor/EditorWindow.swift
@@ -7,7 +7,7 @@
 
 import AppKit
 
-final class EditorWindow: NSWindow {
+final class EditorWindow: NSWindow, @unchecked Sendable {
   var toolbarMode: ToolbarMode? {
     didSet {
       toolbarStyle = toolbarMode == .compact ? .unifiedCompact : .unified
@@ -41,10 +41,13 @@ final class EditorWindow: NSWindow {
 
   override func awakeFromNib() {
     super.awakeFromNib()
-    toolbar = NSToolbar() // Required for multi-tab layout
-    toolbarMode = AppPreferences.Window.toolbarMode
-    tabbingMode = AppPreferences.Window.tabbingMode
-    reduceTransparency = AppDesign.reduceTransparency
+
+    MainActor.assumeIsolated {
+      toolbar = NSToolbar() // Required for multi-tab layout
+      toolbarMode = AppPreferences.Window.toolbarMode
+      tabbingMode = AppPreferences.Window.tabbingMode
+      reduceTransparency = AppDesign.reduceTransparency
+    }
   }
 
   override func layoutIfNeeded() {
