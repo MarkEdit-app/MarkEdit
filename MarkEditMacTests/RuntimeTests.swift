23c909bf15c5dddc3e4440d5cd5b9ee8210fe1d9
Gnohz Gniy <0x00eeee@gmail.com>
2024-10-25 10:42:06 +0800

    Try #4

--- MarkEditMacTests/RuntimeTests.swift
+++ MarkEditMacTests/RuntimeTests.swift
@@ -12,12 +12,4 @@ final class RuntimeTests: XCTestCase {
   func testExistenceOfAppIcon() {
     XCTAssertNotNil(NSImage(named: "AppIcon"), "Missing AppIcon from the main bundle")
   }
-
-  func testPrivateAccessibilityBundles() {
-    let type: AnyClass? = NSObject.axbbmClass
-    XCTAssertNotNil(type, "Missing AXBBundleManager")
-
-    let object = type?.value(forKey: "defaultManager") as? AnyObject
-    XCTAssertEqual(object?.responds(to: sel_getUid("loadAXBundles")), true, "Missing loadAXBundles")
-  }
 }
