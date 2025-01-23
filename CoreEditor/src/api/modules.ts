c211e493a9a31df97552a390dcb588316158fc11
Ying Zhong <0x00eeee@gmail.com>
2025-01-23 09:30:55 +0800

    Integrate markedit-api v0.7.0 for getFileInfo (#827)

--- CoreEditor/src/api/modules.ts
+++ CoreEditor/src/api/modules.ts
@@ -14,6 +14,7 @@ import * as customHistory from '../@vendor/commands/history';
 import { TextEditor } from './editor';
 import {
   onEditorReady,
+  getFileInfo,
   addExtension,
   addMarkdownConfig,
   addCodeLanguage,
@@ -47,6 +48,7 @@ export function initMarkEditModules() {
   MarkEdit.lezer = lezer;
 
   MarkEdit.onEditorReady = onEditorReady;
+  MarkEdit.getFileInfo = getFileInfo;
   MarkEdit.addExtension = addExtension;
   MarkEdit.addMarkdownConfig = addMarkdownConfig;
   MarkEdit.addCodeLanguage = addCodeLanguage;
