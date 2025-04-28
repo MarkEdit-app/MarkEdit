c7acac10d9903b63ba1b6e57fd39480f98f8d36a
Ying Zhong <0x00eeee@gmail.com>
2025-04-28 10:25:14 +0800

    Improve comment parsing w/ the new @lezer/markdown (#893)

--- CoreEditor/src/modules/lezer/index.ts
+++ CoreEditor/src/modules/lezer/index.ts
@@ -1,18 +1,41 @@
-import { SyntaxNodeRef } from '@lezer/common';
-import { parser } from '@lezer/markdown';
+import { parser as htmlParser } from '@lezer/html';
+import { parser as markdownParser } from '@lezer/markdown';
 import { replaceRange } from '../../common/utils';
 import { takePossibleNewline } from '../lineEndings';
 
 export function extractComments(source: string) {
-  const tree = parser.parse(source);
-  const comments: SyntaxNodeRef[] = [];
+  const tree = markdownParser.parse(source);
+  const comments: { from: number; to: number }[] = [];
 
   tree.iterate({
     from: 0, to: source.length,
     enter: node => {
-      if (node.name === 'Comment' || node.name === 'CommentBlock') {
-        comments.push(node.node);
+      if (node.name !== 'Comment' && node.name !== 'CommentBlock') {
+        return;
       }
+
+      const offset = node.from;
+      const html = source.slice(offset, node.to);
+
+      // A "CommentBlock" in Markdown can be something like this:
+      //   <!-- Hello --> World
+      //
+      // The Markdown parser won't extract the "comment" part,
+      // here we need to parse it again using a html parser.
+      htmlParser.parse(html).iterate({
+        from: 0, to: html.length,
+        enter: comment => {
+          if (comment.name !== 'Comment') {
+            return;
+          }
+
+          // Text range with offset from the original Markdown source
+          comments.push({
+            from: comment.from + offset,
+            to: comment.to + offset,
+          });
+        },
+      });
     },
   });
 
