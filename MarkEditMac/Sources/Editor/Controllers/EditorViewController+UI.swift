c8da78b4fe84e868e7964675e6b2086d5fdcc2b5
Ying Zhong <0x00eeee@gmail.com>
2025-06-02 20:08:31 +0800

    Fix issues working with main actors

--- MarkEditMac/Sources/Editor/Controllers/EditorViewController+UI.swift
+++ MarkEditMac/Sources/Editor/Controllers/EditorViewController+UI.swift
@@ -489,7 +489,7 @@ extension EditorViewController {
 // MARK: - Private
 
 private extension EditorViewController {
-  final class UserDefinedMenuItem: NSMenuItem {
+  final class UserDefinedMenuItem: NSMenuItem, @unchecked Sendable {
     var stateGetterID: String?
 
     override var image: NSImage? {
