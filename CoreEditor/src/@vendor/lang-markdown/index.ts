a1751e797d39a5138fa7d42b17b54dd49b3c95c7
Ying Zhong <0x00eeee@gmail.com>
2025-09-28 21:50:29 +0800

    Upgrade dependencies

--- CoreEditor/src/@vendor/lang-markdown/index.ts
+++ CoreEditor/src/@vendor/lang-markdown/index.ts
@@ -1,5 +1,5 @@
 import {Prec, EditorState} from "@codemirror/state"
-import {KeyBinding, keymap} from "@codemirror/view"
+import {KeyBinding, keymap, EditorView} from "@codemirror/view"
 import {Language, LanguageSupport, LanguageDescription, syntaxTree} from "@codemirror/language"
 import {Completion, CompletionContext} from "@codemirror/autocomplete"
 import {MarkdownExtension, MarkdownParser, parseCode} from "@lezer/markdown"
@@ -45,6 +45,10 @@ export function markdown(config: {
   /// completes HTML tags when a `<` is typed. Set this to false to
   /// disable this.
   completeHTMLTags?: boolean,
+  /// The returned language contains
+  /// [`pasteURLAsLink`](#lang-markdown.pasteURLAsLink) as a support
+  /// extension unless you set this to false.
+  pasteURLAsLink?: boolean
   /// By default, HTML tags in the document are handled by the [HTML
   /// language](https://github.com/codemirror/lang-html) package with
   /// tag matching turned off. You can pass in an alternative language
@@ -56,10 +60,12 @@ export function markdown(config: {
        addKeymap = true,
        base: {parser} = commonmarkLanguage,
        completeHTMLTags = true,
+       pasteURLAsLink: pasteURL = true,
        htmlTagLanguage = htmlNoMatch} = config
   if (!(parser instanceof MarkdownParser)) throw new RangeError("Base parser provided to `markdown` should be a Markdown parser")
   let extensions = config.extensions ? [config.extensions] : []
   let support = [htmlTagLanguage.support, headerIndent], defaultCode
+  if (pasteURL) support.push(pasteURLAsLink)
   if (defaultCodeLanguage instanceof LanguageSupport) {
     support.push(defaultCodeLanguage.support)
     defaultCode = defaultCodeLanguage.language
@@ -97,3 +103,34 @@ function htmlTagCompletions() {
   let result = htmlCompletionSource(new CompletionContext(EditorState.create({extensions: htmlNoMatch}), 0, true))
   return _tagCompletions = result ? result.options : []
 }
+
+const nonPlainText = /code|horizontalrule|html|link|comment|processing|escape|entity|image|mark|url/i
+
+/// An extension that intercepts pastes when the pasted content looks
+/// like a URL and the selection is non-empty and selects regular
+/// text, making the selection a link with the pasted URL as target.
+export const pasteURLAsLink = EditorView.domEventHandlers({
+  paste: (event, view) => {
+    let {main} = view.state.selection
+    if (main.empty) return false
+    let link = event.clipboardData?.getData("text/plain")
+    if (!link || !/^(https?:\/\/|mailto:|xmpp:|www\.)/.test(link)) return false
+    if (/^www\./.test(link)) link = "https://" + link
+    if (!markdownLanguage.isActiveAt(view.state, main.from, 1)) return false
+    let tree = syntaxTree(view.state), crossesNode = false
+    // Verify that no nodes are started/ended between the selection
+    // points, and we're not inside any non-plain-text construct.
+    tree.iterate({
+      from: main.from, to: main.to,
+      enter: node => { if (node.from > main.from || nonPlainText.test(node.name)) crossesNode = true },
+      leave: node => { if (node.to < main.to) crossesNode = true }
+    })
+    if (crossesNode) return false
+    view.dispatch({
+      changes: [{from: main.from, insert: "["}, {from: main.to, insert: `](${link})`}],
+      userEvent: "input.paste",
+      scrollIntoView: true
+    })
+    return true
+  }
+})
