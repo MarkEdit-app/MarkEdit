993966d180bc00a5fc8430f84b3b9e1bc410056e
Ying Zhong <0x00eeee@gmail.com>
2025-11-20 20:49:48 +0800

    Add reference links auto completion

---
+++ CoreEditor/src/modules/link/index.ts
@@ -0,0 +1,15 @@
+import { EditorState } from '@codemirror/state';
+
+export function getFootnoteLabels(state: EditorState): string[] {
+  return extractLabels(state, /\[(\^[^\]]+)\]/g);
+}
+
+export function getReferenceLinkLabels(state: EditorState): string[] {
+  return extractLabels(state, /^\[([^\]]+)\]:\s+/gm);
+}
+
+function extractLabels(state: EditorState, regexp: RegExp): string[] {
+  const doc = state.doc.toString();
+  const labels = [...doc.matchAll(regexp)].map(match => match[1]);
+  return [...new Set(labels)];
+}
