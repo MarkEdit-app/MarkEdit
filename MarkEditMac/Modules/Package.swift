52f2dca73bdbe820107a9ec83faaf1ecef1b9101
Ying Zhong <0x00eeee@gmail.com>
2025-11-29 13:30:37 +0800

    swift-tools-version: 6.0

--- MarkEditMac/Modules/Package.swift
+++ MarkEditMac/Modules/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "Modules",
   platforms: [
-    .macOS(.v14),
+    .macOS(.v15),
   ],
   products: [
     .library(
