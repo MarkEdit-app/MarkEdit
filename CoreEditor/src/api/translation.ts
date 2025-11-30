ea7d2cc3559723d3e1ec308e32e6404287014a26
Ying Zhong <0x00eeee@gmail.com>
2025-11-30 11:26:45 +0800

    Resolve comments

--- CoreEditor/src/api/translation.ts
+++ CoreEditor/src/api/translation.ts
@@ -6,11 +6,11 @@ import { TranslationService, TranslationResponse } from 'markedit-api';
 export class Translator implements TranslationService {
   async translate(text: string, languages?: { from?: string; to?: string }): Promise<TranslationResponse> {
     const response = await window.nativeModules.translation.translate({ text, ...languages });
-    const parsed = JSON.parse(response);
-    if (parsed.error !== undefined) {
-      return { succeeded: false, error: parsed.error };
-    } else {
+    const parsed = JSON.parse(response) ?? {};
+    if (typeof parsed.text === 'string') {
       return { succeeded: true, text: parsed.text };
+    } else {
+      return { succeeded: false, error: parsed.error };
     }
   }
 }
