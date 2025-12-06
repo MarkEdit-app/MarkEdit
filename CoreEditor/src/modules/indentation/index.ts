cc2bb3e3c784c347b0d9aea0ec1c85d360dd4c17
Ying Zhong <0x00eeee@gmail.com>
2025-12-06 13:03:41 +0800

    Always indent more around list marks (#1172)

--- CoreEditor/src/modules/indentation/index.ts
+++ CoreEditor/src/modules/indentation/index.ts
@@ -1,6 +1,7 @@
 import { KeyBinding } from '@codemirror/view';
 import { indentLess, indentMore, insertTab } from '@codemirror/commands';
 import { acceptCompletion as acceptTooltipCompletion } from '@codemirror/autocomplete';
+import { syntaxTree } from '@codemirror/language';
 import { TabKeyBehavior } from './types';
 import { hasTooltipCompletion } from '../completion';
 import replaceSelections from '../commands/replaceSelections';
@@ -16,6 +17,17 @@ export const indentationKeymap: KeyBinding[] = [
       if (hasTooltipCompletion()) {
         acceptTooltipCompletion(editor);
         return true;
+      }
+
+      const cursor = editor.state.selection.main.from;
+      const tree = syntaxTree(editor.state);
+
+      // Always indent more around list marks
+      for (const side of [-1, 0, 1]) {
+        const node = tree.resolve(cursor, side as Parameters<typeof tree.resolve>[1]);
+        if (node.name === 'ListItem' || node.name === 'ListMark') {
+          return indentMore(editor);
+        }
       }
 
       switch (window.config.tabKeyBehavior) {
