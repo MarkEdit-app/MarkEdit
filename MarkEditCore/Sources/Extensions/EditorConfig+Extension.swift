8f3ed4233d92545c2579d0fd088853cd443cce16
Ying Zhong <0x00eeee@gmail.com>
2026-03-10 09:28:43 +0800

    Slightly optimize bootstrap (#1272)

--- MarkEditCore/Sources/Extensions/EditorConfig+Extension.swift
+++ MarkEditCore/Sources/Extensions/EditorConfig+Extension.swift
@@ -8,7 +8,7 @@ import Foundation
 
 public extension EditorConfig {
   var toHtml: String {
-    indexHtml?
+    Self.indexHtml?
       .replacingOccurrences(of: "/chunk-loader/", with: "chunk-loader://")
       .replacingOccurrences(of: "\"{{EDITOR_CONFIG}}\"", with: jsonEncoded) ?? ""
   }
@@ -16,11 +16,11 @@ public extension EditorConfig {
 
 extension EditorConfig {
   /// index.html built by CoreEditor.
-  private var indexHtml: String? {
+  private static let indexHtml: String? = {
     guard let path = Bundle.main.url(forResource: "index", withExtension: "html") else {
       fatalError("Missing dist/index.html to set up the editor. In the wiki, see Building CoreEditor.")
     }
 
     return try? Data(contentsOf: path).toString()
-  }
+  }()
 }
