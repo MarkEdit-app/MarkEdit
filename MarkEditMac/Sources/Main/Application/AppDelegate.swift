82950395880922fa1cf8943a3e1bb5ccbf734ae7
Ying Zhong <0x00eeee@gmail.com>
2026-06-24 18:54:02 +0800

    Use validateMenuItem for "New file from Clipboard" (#1512)

--- MarkEditMac/Sources/Main/Application/AppDelegate.swift
+++ MarkEditMac/Sources/Main/Application/AppDelegate.swift
@@ -30,7 +30,6 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
   @IBOutlet weak var lineEndingsLFItem: NSMenuItem?
   @IBOutlet weak var lineEndingsCRLFItem: NSMenuItem?
   @IBOutlet weak var lineEndingsCRItem: NSMenuItem?
-  @IBOutlet weak var fileFromClipboardItem: NSMenuItem?
   @IBOutlet weak var fileNewTabItem: NSMenuItem?
   @IBOutlet weak var fileReopenClosedTabItem: NSMenuItem?
   @IBOutlet weak var editUndoItem: NSMenuItem?
@@ -170,18 +169,6 @@ extension AppDelegate {
         break
       }
     }
-  }
-}
-
-// MARK: - NSMenuItemValidation
-
-extension AppDelegate: NSMenuItemValidation {
-  func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
-    if menuItem.action == #selector(reopenClosedTab(_:)) {
-      return EditorClosedTabHistory.shared.hasEntries
-    }
-
-    return true
   }
 }
 
