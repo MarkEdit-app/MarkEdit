d11611b87891828295be0bc170451ccc63d33aa3
Ying Zhong <0x00eeee@gmail.com>
2026-07-17 15:51:53 +0800

    Add NSTextField.alertCapableTextField

--- MarkEditMac/Modules/Sources/AppKitExtensions/UI/NSTextField+Extension.swift
+++ MarkEditMac/Modules/Sources/AppKitExtensions/UI/NSTextField+Extension.swift
@@ -7,6 +7,10 @@
 import AppKit
 
 public extension NSTextField {
+  static var alertCapableTextField: Self {
+    Self(frame: CGRect(x: 0, y: 0, width: 256, height: 22))
+  }
+
   func startEditing(in window: NSWindow?, alwaysRefocus: Bool = false) {
     guard alwaysRefocus || !isFirstResponder(in: window) else {
       return
