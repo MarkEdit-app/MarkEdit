1671b8898e38873ee057738351b803e7f69fc7e4
Ying Zhong <0x00eeee@gmail.com>
2026-08-06 10:11:03 +0800

    Strict concurrency changes (#1666)

--- MarkEditMac/Sources/Main/AppWritingTools.swift
+++ MarkEditMac/Sources/Main/AppWritingTools.swift
@@ -10,6 +10,7 @@ import AppKitExtensions
 import MarkEditKit
 
 @available(macOS 15.1, *)
+@MainActor
 enum AppWritingTools {
   // Source/WebKit/Platform/spi/Cocoa/Modules/WritingTools_SPI/WritingToolsSPI.h
   enum Tool: Int {
